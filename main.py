3ec03f0a08fb59f62fe88c1071cc0bd4baf2e3c2
Ru Zhu <rogxeon@gmail.com>
2017-02-19 10:23:33 -0800

    revised comment

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ class Chat:
         #chatFrame
         self.chatHistory = Text(chatFrame, bg='grey', height=10, width=40, state=DISABLED)
         self.chatHistory.grid(row=0, column=0, sticky=W+N+S)
-#this is a comment
+# this is a longgggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg comment
 
         self.textBox = Entry(chatFrame)
         self.textBox.grid(row=1, sticky=E+W)
